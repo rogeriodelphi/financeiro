f605ef57fcfed618332b366f1872c493eb88ae3c
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-10 20:44:05 -0300

    Adicionando o Procfile ao projeto

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: run-program gunicorn financeiro.wsgi --log-file -
--- financeiro/settings.py
+++ financeiro/settings.py
@@ -72,11 +72,22 @@ WSGI_APPLICATION = 'financeiro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'financeirodb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
